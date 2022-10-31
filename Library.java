9170362f8c1bceff359a90e8b5761a76a4262958
JOANNA LI <jli40@smith.edu>
2022-10-31 15:39:39 -0400

    IsAvailable and containsTitle methods implemented

--- Library.java
+++ Library.java
@@ -13,20 +13,34 @@ public class Library extends Building {
   
     public void addTitle(String title) {
       collection.put(title, true);
+      System.out.println("You have added " + title + " to the collection");
+
     }
 
     public String removeTitle(String title) {
       collection.remove(title);
+      System.out.println("You have removed " + title + " from the collection");
       return title;
     }
 
     public void checkOutTitle(String title) {
-      collection.replace(title, true, false); 
+      collection.replace(title, true, false);
+      System.out.println("You have checked out " + title);
     }
     
     public void returnTitle(String title) {
       collection.replace(title, false, true);
+      System.out.println("You have returned " + title);
     }
+
+    public boolean containsTitle(String title) {
+      return collection.containsKey(title);
+    }
+
+    public boolean isAvailable(String title) {
+      return collection.get(title);
+    }
+
 
     public static void main(String[] args) {
 
@@ -35,12 +49,21 @@ public class Library extends Building {
       //Add a book
       neilsonLibrary.addTitle("The Wealth of Nations by Adam Smith");
       //Check if book was added 
-      System.out.println(neilsonLibrary.collection.containsKey("The Wealth of Nations by Adam Smith"));
-      
+      System.out.println(neilsonLibrary.containsTitle("The Wealth of Nations by Adam Smith"));
+
+      //Check out the book 
+      neilsonLibrary.checkOutTitle("The Wealth of Nations by Adam Smith");
+      System.out.println(neilsonLibrary.isAvailable("The Wealth of Nations by Adam Smith"));
+
+      //Return the book 
+      neilsonLibrary.returnTitle("The Wealth of Nations by Adam Smith");
+      System.out.println(neilsonLibrary.isAvailable("The Wealth of Nations by Adam Smith"));
+
       //Remove the book
-      neilsonLibrary.removeTitle("The Wealth of Nations by Adam Smith"); 
+      neilsonLibrary.removeTitle("The Wealth of Nations by Adam Smith");
+
       //Check if book was removed 
-      System.out.println(neilsonLibrary.collection.containsKey("The Wealth of Nations by Adam Smith"));
+      System.out.println(neilsonLibrary.containsTitle("The Wealth of Nations by Adam Smith"));
     }
   
   }
