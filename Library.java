c0182912df91947fc56a35ac7956f28880593dad
JOANNA LI <jli40@smith.edu>
2022-10-31 15:05:54 -0400

    Library methods to add, remove, check out, and return books were implemented

--- Library.java
+++ Library.java
@@ -2,7 +2,7 @@ import java.util.Hashtable;
 
 public class Library extends Building {
 
-    private Hashtable<String, Boolean> collection;  
+    private Hashtable<String, Boolean> collection;
 
     public Library(String name, String address, int nFloors, Hashtable<String, Boolean> collection) {
       super(name, address, nFloors);
@@ -11,11 +11,36 @@ public class Library extends Building {
       System.out.println("You have built a library");
     }
   
-    public static void main(String[] args) {
-      
-      Hashtable<String, Boolean> collection = new Hashtable<String, Boolean>();
+    public void addTitle(String title) {
+      collection.put(title, true);
+    }
 
-      new Library("Neilson Library", "Neilson Drive", 4, collection);
+    public String removeTitle(String title) {
+      collection.remove(title);
+      return title;
+    }
+
+    public void checkOutTitle(String title) {
+      collection.replace(title, true, false); 
+    }
+    
+    public void returnTitle(String title) {
+      collection.replace(title, false, true);
+    }
+
+    public static void main(String[] args) {
+
+      Library neilsonLibrary =  new Library("Neilson Library", "Neilson Drive", 4, new Hashtable<String, Boolean>());
+      
+      //Add a book
+      neilsonLibrary.addTitle("The Wealth of Nations by Adam Smith");
+      //Check if book was added 
+      System.out.println(neilsonLibrary.collection.containsKey("The Wealth of Nations by Adam Smith"));
+      
+      //Remove the book
+      neilsonLibrary.removeTitle("The Wealth of Nations by Adam Smith"); 
+      //Check if book was removed 
+      System.out.println(neilsonLibrary.collection.containsKey("The Wealth of Nations by Adam Smith"));
     }
   
   }
