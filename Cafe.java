89035a5df5754e0c4867fb179a1dee4811cc7761
JOANNA LI <jli40@smith.edu>
2022-10-31 21:33:46 -0400

    sellCoffee() method implemented

--- Cafe.java
+++ Cafe.java
@@ -12,9 +12,24 @@ public class Cafe extends Building {
         this.nCreams = nCreams; 
         this.nCups = nCups; 
     }
+
+    public void sellCoffee(int nCoffeeOunces, int nSugarPackets, int nCreams) {
+        this.nCups -= 1; 
+        this.nCoffeeOunces -= nCoffeeOunces; 
+        this.nSugarPackets -= nSugarPackets; 
+        this.nCreams -= nCreams; 
+    }
     
     public static void main(String[] args) {
-        new Cafe("Compass Cafe", "Neilson Drive", 1, 10, 10, 10, 10);
+        Cafe compassCafe = new Cafe("Compass Cafe", "Neilson Drive", 1, 10, 10, 10, 10);
+        
+        //Testing sellCoffee()
+        System.out.println(compassCafe.nCups);
+        compassCafe.sellCoffee(5, 4, 3);
+        System.out.println(compassCafe.nCups);
+        System.out.println(compassCafe.nCoffeeOunces);
+        System.out.println(compassCafe.nSugarPackets);
+        System.out.println(compassCafe.nCreams);
     }
     
 }
