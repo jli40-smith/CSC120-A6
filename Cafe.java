9a3284fbbe37b607a7d5940f7cb0142b8714ccd2
JOANNA LI <jli40@smith.edu>
2022-10-31 21:16:19 -0400

    Added requested attributes

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,20 @@
-/* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building {
+    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets; // The number of sugar packets remaining in inventory
+    private int nCreams; // The number of "splashes" of cream remaining in inventory
+    private int nCups; // The number of cups remaining in inventory
 
-    public Cafe() {
-        System.out.println("You have built a cafe: ☕");
+    public Cafe(String name, String address, int nFloors, int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        super(name, address, nFloors); 
+        System.out.println("You have built a cafe");
+        this.nCoffeeOunces = nCoffeeOunces; 
+        this.nSugarPackets = nSugarPackets; 
+        this.nCreams = nCreams; 
+        this.nCups = nCups; 
     }
     
     public static void main(String[] args) {
-        new Cafe();
+        new Cafe("Compass Cafe", "Neilson Drive", 1, 10, 10, 10, 10);
     }
     
 }
